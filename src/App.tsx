63d58ae858347cf57ff377a14e262b0158ff0d4c
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-03-07 12:07:22 -0300

    Botao

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import Formulario from './components/Formulario';
+import Lista from './components/Lista';
+
 
 
 function App() {
   return (
     <div className="App">
-     
+     <Formulario />
+     <Lista />
     </div>
   );
 }
