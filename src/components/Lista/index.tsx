952f6c8e47d2873d186efdc6d33a0dbfbe13a463
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-03-07 12:10:48 -0300

    Lista

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Lista() {
+  const tarefas = [{
+    tarefa: 'React',
+    tempo: '02:00:00'
+  }, 
+  {
+    tarefa: 'JavaScript',
+    tempo: '01:00:00'
+  },
+  {
+    tarefa: 'Typescript',
+    tempo: '02:00:00'
+  },
+  {
+    tarefa: 'Python',
+    tempo: '01:30:00'
+  }, {
+    tarefa: 'C#',
+    tempo: '03:00:00'
+  }]  
+  return (
+    <aside>
+      <h2>Estudos do dia</h2>
+      <ul>
+        {tarefas.map((item, index) => (
+          <li key={index}>
+            <h3>{item.tarefa}</h3>
+            <span>{item.tarefa}</span>
+          </li>
+        ))}
+        
+      </ul>
+    </aside>
+  );
+}
+
+export default Lista;
