cf7f359e4d167fc352f6ceffa02dad2703d2e4a1
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-03-08 00:11:54 -0300

    Tipando props Botao

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,14 +2,15 @@ import style from './Botao.module.scss'
 import React from 'react';
 
 interface props {
-  children: string
+  children: string,
+  type?: 'button' | 'submit' | 'reset' | undefined
 }
-
 
 class Botao extends React.Component<props> {
   render() {
+    const { type= 'button' } = this.props
     return (
-      <button className={style.botao}>
+      <button type={type} className={style.botao}>
         {this.props.children}
       </button>
     )
