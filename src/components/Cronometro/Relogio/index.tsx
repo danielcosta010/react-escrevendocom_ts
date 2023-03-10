c5fa530768b29252be60cb718dcabcd127647aa4
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-03-09 21:51:28 -0300

    Update  index.tsx Cronometro

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,21 @@
 import style from './Relogio.module.scss';
 
-function Relogio() {
+interface Props {
+  tempo: number | undefined
+}
+
+function Relogio({ tempo = 0 }: Props) {
+  const minituto = Math.floor(tempo / 60);
+  const segundo = tempo - minituto * 60;
+  const [minutoDezena, minutoUnidade] = String(minituto).padStart(2, '0');
+  const [segundoDezena, segundoUnidade] = String(segundo).padStart(2, '0');
   return (  
     <>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{minutoDezena}</span>
+      <span className={style.relogioNumero}>{minutoUnidade}</span>
       <span className={style.relogioDivisao}>:</span>
-      <span className={style.relogioNumero}>0</span>
-      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>{segundoDezena}</span>
+      <span className={style.relogioNumero}>{segundoUnidade}</span>
     </>
   );
 }
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -2,15 +2,42 @@ import style from './Cronometro.module.scss'
 
 import Botao from "../Botao";
 import Relogio from "./Relogio";
+import { tempoParaSegundos } from '../../common/utils/time';
+import { ITarefa } from '../../Types/Tarefa';
+import { useEffect, useState } from 'react';
 
-function Cronometro() {
+
+interface Props {
+  selecionado: ITarefa | undefined
+}
+
+function Cronometro({ selecionado }: Props) {
+  
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+      }
+  }, [selecionado]);
+
+  function regressiva(contador: number = 0) {
+    setTimeout(() => {
+      if (contador > 0) {
+        setTempo(contador - 1);
+        return regressiva(contador - 1);
+      }
+    }, 1000);
+  }
+
   return (  
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o tempo</p>
       <div className={style.relogioWrapper}>
-        <Relogio />
+        <Relogio tempo={tempo}/>  
+        
       </div>
-      <Botao type='button'>
+      <Botao onClick={() => regressiva(tempo)}>
         Começar!
       </Botao>
     </div>
