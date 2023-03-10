a5c0ef601cacac94ff7608ca3cd823622af7e624
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-03-09 21:52:22 -0300

    Update index.tsx

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,5 +1,5 @@
 import { ITarefa } from '../../../Types/Tarefa';
-import style from '../../Lista/Lista.module.scss';
+import style from './Item.module.scss';
 
 interface Props extends ITarefa {
   selecionaTarefa: (tarefaSelecionada: ITarefa) => void
@@ -14,7 +14,7 @@ function Item(
     id, 
     selecionaTarefa 
   }: Props) {
-  console.log('item atual:', { tarefa, tempo, selecionado, completado, id });
+  
   
   return (  
     <li 
