1775351a99dd366c520821cc0b3b00404c252507
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-03-09 21:50:42 -0300

    Update index.tsx Botao

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,15 +2,18 @@ import style from './Botao.module.scss'
 import React from 'react';
 
 interface props {
-  children: string,
-  type?: 'button' | 'submit' | 'reset' | undefined
+  
 }
 
-class Botao extends React.Component<props> {
+class Botao extends React.Component<{
+  children: string,
+  type?: 'button' | 'submit' | 'reset' | undefined
+  onClick?: () => void
+}> {
   render() {
-    const { type= 'button' } = this.props
+    const { type= 'button', onClick } = this.props
     return (
-      <button type={type} className={style.botao}>
+      <button onClick={onClick} type={type} className={style.botao}>
         {this.props.children}
       </button>
     )
